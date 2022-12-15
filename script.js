4767e1a06ef8ae722c84823c0e5d5a8d9c18a163
Anano Topuria <anano.topuriaa@gmail.com>
2022-12-15 21:40:30 +0400

    implementing a countdown timer

--- script.js
+++ script.js
@@ -126,6 +126,13 @@ const formatMovementDate = function (date, locale) {
   return new Intl.DateTimeFormat(locale).format(date);
 };
 
+const formatCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: `currency`,
+    currency: currency,
+  }).format(value);
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = ``;
   const movs = sort
@@ -136,12 +143,14 @@ const displayMovements = function (acc, sort = false) {
     const date = new Date(acc.movementsDates[i]);
     const displayDate = formatMovementDate(date, acc.locale);
 
+    const formattedMov = formatCur(mov, acc.locale, acc.currency);
+
     const html = `
     <div class="movements__row">
       <div class="movements__type 
       movements__type--${type}">${i + 1} ${type}</div>
       <div class="movements__date">${displayDate}</div>
-      <div class="movements__value">${mov.toFixed(2)}€</div>
+      <div class="movements__value">${formattedMov}</div>
   </div>
   `;
     containerMovements.insertAdjacentHTML(`afterbegin`, html);
@@ -150,18 +159,18 @@ const displayMovements = function (acc, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance.toFixed(2)} €`;
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out).toFixed(2)} €`;
+  labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);
   const interest = acc.movements
     .filter(mov => mov > 0)
     .map(deposit => (deposit * acc.interestRate) / 100)
@@ -170,7 +179,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
+  labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
 };
 
 const createUsernames = function (accs) {
@@ -196,18 +205,41 @@ const updateUI = function (acc) {
 // const account = accounts.find(acc => acc.owner === `Jessica Davis`);
 // console.log(account);
 
-//Logins
+const startLogOutTimer = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+    //in each callback call pront the remaining time to UI
+
+    labelTimer.textContent = `${min}:${sec}`;
+
+    // when 0 sec, stop timer and log out user
+    if (time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = `Log in to get started`;
+      containerApp.style.opacity = 0;
+    }
+    // Decrease 1s
+
+    time--;
+  };
+
+  // Set time to 5 minuters
+  let time = 30;
+  //call timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+  return timer;
+};
+
 // Event handler
 
-let currentAccount;
+let currentAccount, timer;
 
 // Fake always logged in
-
-currentAccount = account1;
-
-updateUI(currentAccount);
-containerApp.style.opacity = 100;
-
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 btnLogin.addEventListener(`click`, function (e) {
   // prevent form from submitting
   e.preventDefault();
@@ -247,10 +279,14 @@ btnLogin.addEventListener(`click`, function (e) {
     // const hour = `${now.getHours()}`.padStart(2, `0`);
     // const min = `${now.getMinutes()}`.padStart(2, `0`);
     // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
-
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = ``;
     inputLoginPin.blur();
+
+    //timer
+    if (timer) clearInterval(timer);
+    timer = startLogOutTimer();
+
     // update UI
     updateUI(currentAccount);
   }
@@ -283,6 +319,10 @@ btnTransfer.addEventListener(`click`, function (e) {
     //Update UI
 
     updateUI(currentAccount);
+
+    // Reset TIMER
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 
@@ -292,14 +332,19 @@ btnLoan.addEventListener(`click`, function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    //  Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      //  Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+      // Reset TIMER
+      clearInterval(timer);
+      timer = startLogOutTimer();
+    }, 2500);
   }
   inputLoanAmount.value = ``;
 });
